345605fd9e9cc6fd5c9dfeb55be6185fe6f914ad
Mohammad Ashif <mohammadashif510@gmail.com>
2026-06-17 13:03:48 +0600

    check auth code updated

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -3,42 +3,58 @@ import jwt, { type JwtPayload } from "jsonwebtoken";
 import { config } from "../config";
 import { pool } from "../../db";
 
+
 export const checkAuth =
-    (...authRoles: any) =>
+    (...authRoles: string[]) =>
     async (req: Request, res: Response, next: NextFunction) => {
-        const accessToken = req.headers.authorization;
-        if (!accessToken) {
-            throw new Error("You are not authorized");
-        }
-        const verifyToken = jwt.verify(
-            accessToken,
-            config.JWT_SECRET as string,
-        ) as JwtPayload;
+        try {
+            const accessToken = req.headers.authorization;
+            if (!accessToken) {
+                res.status(401).json({
+                    success: false,
+                    message: "You are not authorized",
+                });
+                return;
+            }
 
-        const userData = await pool.query(
-            `
-            SELECT * FROM users WHERE id = $1
-            `,
-            [verifyToken.id],
-        );
+            const verifyToken = jwt.verify(
+                accessToken,
+                config.JWT_SECRET as string,
+            ) as JwtPayload;
 
-        const user = userData.rows[0];
+            const userData = await pool.query(
+                `
+                SELECT * FROM users WHERE id = $1
+                `,
+                [verifyToken.id],
+            );
 
-        if (userData.rows.length === 0) {
-            res.status(404).json({
+            if (userData.rows.length === 0) {
+                res.status(404).json({
+                    success: false,
+                    message: "User not found!",
+                });
+                return;
+            }
+
+            const user = userData.rows[0];
+
+            if (authRoles.length && !authRoles.includes(user.role)) {
+                res.status(403).json({
+                    success: false,
+                    message: "Forbidden",
+                });
+                return;
+            }
+
+            req.user = verifyToken;
+
+            next();
+        } catch (error: any) {
+            res.status(401).json({
                 success: false,
-                message: "User not found!",
+                message: "Invalid expired token",
+                error: error.message,
             });
         }
-
-        if (!authRoles.length && !authRoles.includes(user.role)) {
-            res.status(403).json({
-                success: false,
-                message: "Forbidden",
-            });
-        }
-
-        req.user = verifyToken;
-
-        next();
     };
